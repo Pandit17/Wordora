a1fb71434035d183ba55b248c146e2759c0d63bf
Shwet Gautam <gautamshwet17@gmail.com>
2025-10-28 03:14:27 +0530

    Configured GitHub Pages deployment and updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Configuration for Vite + React + GitHub Pages
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // Important: this tells GitHub Pages where your app is served from
+  base: "/Wordora/",
+});
